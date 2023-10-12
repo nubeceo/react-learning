136e3143dc3a8cd6fb5347c09116861f77f1e836
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-10-12 12:16:24 +0530

    updated

--- 01veetreact/src/main.jsx
+++ 01veetreact/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>hello ji!</h1>
+    </div>
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MyApp/>
+  </React.StrictMode>
 )
