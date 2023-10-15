9cbfa3d8fe93f4f52c8eaa8f5691188a4ef369da
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-10-15 10:30:32 +0530

    updated

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj={
+    username: "hitesh",
+    age:21
+  }
+
+  let newArr =[1,2,3,4];
+
   return (
     <>
-      <h1 className='bg-green-400'>tailwind test</h1>
+      <h1 className='bg-green-400 mb-4'>tailwind test</h1>
+      <Card username = "chaiaurcode" />
     </>
   )
 }
