4389f4ed285ebfb790318706f7a8e42e13fa2b31
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-10-15 20:13:38 +0530

    updated

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -2,14 +2,43 @@ import { useState } from "react"
 
 
 function App() {
-  const [color, setColor]=useState("grey")
+  const [color, setColor] = useState("grey");
 
-
+  
   return (
     <>
-      <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
-      
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0"></div>
+      <div className="w-full h-screen duration-200 flex justify-center items-center" style={{ backgroundColor: color }}>
+
+
+        <div className="font-black text-4xl"></div>
+
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
+
+
+
+          <div  className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl ">
+
+            <button  onClick={() => setColor("red")} className="outline-none px-4 font-black py-1 rounded-3xl bg-red-700 ">RED</button>
+
+
+
+            <button onClick={() => setColor("blue")} className="outline-none px-4 font-black py-1 rounded-3xl bg-blue-700">BLUE</button>
+
+
+
+            <button onClick={() => setColor("green")} className="outline-none px-4 font-black py-1 rounded-3xl bg-green-700">GREEN</button>
+
+
+
+            <button onClick={() => setColor("yellow")} className="outline-none px-4 font-black py-1 rounded-3xl bg-yellow-400">YELLOW</button>
+
+
+
+            <button onClick={() => setColor("purple")} className="outline-none px-4 font-black py-1 rounded-3xl bg-purple-700">PURPLE</button>
+          </div>
+
+
+        </div>
 
       </div>
 
