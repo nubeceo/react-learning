ded88047e97059f029cdf452f9c3c4b69f264094
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-10-19 11:43:30 +0530

    updated

---
+++ practiceCurrency/src/Components/InputBox.jsx
@@ -0,0 +1,10 @@
+import React,{useEffect,useState} from "react";
+
+
+function InputBox(){
+
+
+
+}
+
+export default InputBox;
---
+++ practiceCurrency/src/Hooks/CurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function CurrencyInfo(currency) {
+
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+
+            .then((response) => response.json())
+            .then((response) => setData(response[currency]))
+
+    }, [currency])
+
+    return data;
+}
+
+export default CurrencyInfo;
