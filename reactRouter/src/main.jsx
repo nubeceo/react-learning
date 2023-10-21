0ef2dce70934f298fe065dca162b769222b0914f
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-10-21 16:51:20 +0530

    updated

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,20 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+
 
 const router = createBrowserRouter(
 
   createRoutesFromElements(
 
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+    </Route>
     
   )
 
 )
 
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
