f7c0a1fecc6ab955c7a27c8132029322057a08bc
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-10-21 10:14:56 +0530

    updated

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header/Header'
+import Home from './Components/Home/Home'
+import Footer from './Components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+    {/* <Header/>
+    <Home/>
+    <Footer/> */}
     
     </>
   )
--- reactRouter/src/Components/Header/Header.jsx
+++ reactRouter/src/Components/Header/Header.jsx
@@ -40,11 +40,11 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    className={({isActive}) =>
+                                    className={({ isActive }) =>
                                         `block py-2 
                                         pr-4 pl-3 
                                         duration-200 
-                                        ${isActive ?"text-orange-700":"text-gray-700"}
+                                        ${isActive ? "text-orange-700" : "text-gray-700"}
                                         border-b
                                      border-gray-100 
                                      hover:bg-gray-50 lg:hover:bg-transparent 
