99066d9a114bdde2edfa05fd533fe6cc79d415f7
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2024-01-26 23:38:20 +0530

    updated

--- 01veetreact/src/App.jsx
+++ 01veetreact/src/App.jsx
@@ -1,28 +1,11 @@
 import { useState } from "react";
 
 
-const game = ["football", "basketball", "cricket"];
-const days = ["weekends", "weekdays"];
-
 const [gameplay, setGameplay] = useState("");
 function App() {
   return (
-    <>
-      <h1>which game?</h1>
-      {
-        game.map((value) => {
-          return (
-            <>
-              <input value={value} onChange={setGameplay((e)=>{e.target.value})}  type="radio" id={value} />
-
-              <label for={value} value={value}>{value}</label>
-              <br /><br />
-            </>
-          )
-        })
-      }
-
-
+    < >
+      
     </>
   )
 }
---
+++ interview/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [phNumber, setphNumber] = useState("");
+  const [email, setemail] = useState("");
+
+  const emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+
+  const isEmailValid = () => {
+    return emailRegex.test(email);
+  }
+
+
+  const handelSubmit = () => {
+
+    if (lastName == "" && firstName == "" && phNumber == "") {
+      alert("field cant be blank");
+    }
+
+    if (firstName == "") {
+      alert("first name cant be blank");
+    }
+    if (lastName == "") {
+      alert("last name cant be blank");
+    }
+
+    if (phNumber == "") {
+      alert("number cant be blank")
+    }
+    if (phNumber.length < 10) {
+      alert("phone cant be less that 10")
+    }
+
+    if (email == "") {
+      alert("email cant be blank")
+    }
+    
+    if(!isEmailValid()){
+      alert("enter a valid email");
+    }
+
+
+  }
+
+  return (
+    <div>
+      helo
+      <div >
+        <input type="text" placeholder='First Name' onChange={(e) => setfirstName(e.target.value)} value={firstName} />
+        <input type="text" placeholder='Last Name' onChange={(e) => setlastName(e.target.value)} value={lastName} />
+        <input type="tel" placeholder='ph no' onChange={(e) => setphNumber(e.target.value)} value={phNumber} />
+        <input type="email" placeholder='email' onChange={(e) => setemail(e.target.value)} value={email} />
+      </div>
+
+      <div>
+        <button onClick={() => handelSubmit()}>Submit</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
