8ad2eb3a5a873d8fb126a1aeed986f330b453c7d
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2024-01-27 20:25:01 +0530

    updated app.jsx

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { todoProvider, } from './Context'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [todo, setTodo] = useState([])
 
   return (
-    <div className='bg-slate-900 w-full h-[100vh] text-white p-10'>
-      ekwjek
-    </div>
+    <todoProvider value={{todos, addtodo, updateTodo, deleteTodo, toggleComple  }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+
+          <div className="mb-4">
+            {/* Todo form goes here */}
+
+          </div>
+
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+
+          </div>
+
+        </div>
+      </div>
+    </todoProvider>
   )
 }
 
