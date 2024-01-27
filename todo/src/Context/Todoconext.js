c154a4334a9754302171d7c582b82df28bbf4f3e
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2024-01-27 20:10:53 +0530

    updated todocontext.js

--- todo/src/Context/Todoconext.js
+++ todo/src/Context/Todoconext.js
@@ -1,6 +1,31 @@
-import React,{ createContext, } from "react";
+import { createContext, useContext, } from "react";
 
-const todoContext = createContext();
 
-export default todoContext;
+
+export const todoContext = createContext({
+
+    todos: [
+
+        {
+            id: 1,
+            todoMsg: "hello ji",
+            completed: false,
+
+        },
+
+
+    ],
+    adTodo: (todo) => { },
+    updateTodo: (id, todo) => { },
+    deleteTodo: (id) => { },
+    toggleComple: (id) => { }
+
+});
+
+
+export const useTodo = () => {
+    return useContext(todoContext)
+}
+
+export const todoProvider = todoContext.Provider
 
